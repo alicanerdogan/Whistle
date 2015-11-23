72a1dd7441f928eef38124e67d28321ebfce9b9d
alicanerdogan <aerdogan07@gmail.com>
2015-11-23 23:43:46 +0200

    DOMSelector Initial Commit

---
+++ Whistle/DOMSelector/Selector.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsQuery;
+
+namespace DOMSelector
+{
+    public static class Selector
+    {
+        public static string GetSelection(string innerHTML, string selector)
+        {
+            string selection = String.Empty;
+
+            CQ domStructure = CsQuery.CQ.Create(innerHTML);
+            domStructure = new CQ(selector, domStructure);
+
+            if (domStructure.Selection.Count() > 0)
+            {
+                selection = domStructure.Selection.First().InnerHTML;
+            }
+            else
+            {
+                throw new NullReferenceException("Invalid selector is given. Nothing has been found.");
+            }
+
+            return selection;
+        }
+    }
+}
