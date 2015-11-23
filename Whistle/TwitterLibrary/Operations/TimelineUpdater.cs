e9cdf1319b41128c556491a73a43ba7683b4b2ea
alicanerdogan <aerdogan07@gmail.com>
2015-11-23 23:46:49 +0200

    Initial commit.

---
+++ Whistle/TwitterLibrary/Operations/TimelineUpdater.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TweetSharp;
+
+namespace TwitterLibrary.Operations
+{
+    public class TimelineUpdater : APIOperation
+    {
+        private ListTweetsOnHomeTimelineOptions Options { get; set; }
+
+        public TimelineUpdater(TwitterService service) : base(service)
+        {
+            Options = new ListTweetsOnHomeTimelineOptions() { Count = 1, ExcludeReplies = false };
+        }
+
+        public IEnumerable<TwitterStatus> Update()
+        {
+            return Service.ListTweetsOnHomeTimeline(Options);
+        }
+    }
+}
