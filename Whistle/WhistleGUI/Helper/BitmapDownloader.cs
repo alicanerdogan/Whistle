f66ac2f4c5ba9a8c0ac581fa7f25f7222a1b4d11
alicanerdogan <aerdogan07@gmail.com>
2015-12-06 11:50:38 +0200

    Initial commit.

---
+++ Whistle/WhistleGUI/Helper/BitmapDownloader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Net;
+using System.Windows.Media.Imaging;
+using System.Net.Cache;
+using System.IO;
+
+namespace WhistleGUI.Helper
+{
+    public static class BitmapDownloader
+    {
+        public static async Task<BitmapImage> DownloadAsync(string url)
+        {
+            var bitmap = new BitmapImage();
+
+            using (var client = new WebClient())
+            {
+                var data = await client.DownloadDataTaskAsync(url);
+                MemoryStream imageStream = new MemoryStream(data);
+
+                bitmap.BeginInit();
+                bitmap.StreamSource = imageStream;
+                bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                bitmap.EndInit();
+                bitmap.Freeze();
+            }
+
+            return bitmap;
+        }
+    }
+}
---
+++ Whistle/WhistleGUI/Helper/VisualTreeTraverser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WhistleGUI.Helper
+{
+    public static class VisualTreeTraverser
+    {
+        private static Visual GetDescendantByType(Visual element, Type type)
+        {
+            if (element == null)
+            {
+                return null;
+            }
+            if (element.GetType() == type)
+            {
+                return element;
+            }
+            Visual foundElement = null;
+            if (element is FrameworkElement)
+            {
+                (element as FrameworkElement).ApplyTemplate();
+            }
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(element); i++)
+            {
+                Visual visual = VisualTreeHelper.GetChild(element, i) as Visual;
+                foundElement = GetDescendantByType(visual, type);
+                if (foundElement != null)
+                {
+                    break;
+                }
+            }
+            return foundElement;
+        }
+
+        public static ScrollViewer GetScrollViewerInsideListBox(ListBox listBox)
+        {
+            return Helper.VisualTreeTraverser.GetDescendantByType(listBox, typeof(ScrollViewer)) as ScrollViewer;
+        }
+    }
+}
