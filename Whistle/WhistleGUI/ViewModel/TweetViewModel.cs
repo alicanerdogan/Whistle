5bda7b301bee35332592d376cb9f8423f8e8f803
alicanerdogan <aerdogan07@gmail.com>
2015-12-06 11:41:53 +0200

    Avatar has been added as a property. TweetSharp dependency is removed by using TwitterLibrary.Tweet as the source.

--- Whistle/WhistleGUI/ViewModel/TweetViewModel.cs
+++ Whistle/WhistleGUI/ViewModel/TweetViewModel.cs
@@ -1,30 +1,49 @@
 ﻿using ReactiveUI;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Cache;
 using System.Text;
 using System.Threading.Tasks;
-using TweetSharp;
+using System.Windows.Media.Imaging;
+using TwitterLibrary;
 
 namespace WhistleGUI.ViewModel
 {
     public class TweetViewModel : ReactiveObject
     {
         #region Properties
-        public TwitterStatus Tweet { get; private set; }
-        public string Content { get { return Tweet.Text; } }
-        public string Username { get { return Tweet.User.Name; } }
-        public string TimeTag { get { return Tweet.CreatedDate.ToString(); } }
-        public bool IsLiked { get { return Tweet.IsFavorited; } }
+        public Tweet Tweet { get; private set; }
+        public string Content { get { return Tweet.RawContent; } }
+        public string Username { get { return Tweet.Owner.Name; } }
+        public string TimeTag { get { return Tweet.RelativeTime; } }
+        public bool IsLiked { get { return false; } }
         public bool IsRetweeted { get { return false; } }
         public int RetweetCount { get { return Tweet.RetweetCount; } }
-        public int ReplyCount { get { return 0; } }
-        public int LikeCount { get { return 0; } }
+        public int ReplyCount { get { return Tweet.ReplyCount; } }
+        public int LikeCount { get { return Tweet.LikeCount; } }
+
+        private BitmapImage _Avatar;
+        public BitmapImage Avatar
+        {
+            get { return _Avatar; }
+            set { this.RaiseAndSetIfChanged(ref _Avatar, value); }
+        }
         #endregion
 
-        public TweetViewModel(TwitterStatus status)
+        public TweetViewModel(Tweet tweet)
         {
-            Tweet = status;
+            Tweet = tweet;
+            LoadAvatar(Tweet.Owner.AvatarURL);
+        }
+
+        private async void LoadAvatar(string url)
+        {
+            var bitmap = await Helper.BitmapDownloader.DownloadAsync(url);
+            Avatar = bitmap;
         }
     }
 }
