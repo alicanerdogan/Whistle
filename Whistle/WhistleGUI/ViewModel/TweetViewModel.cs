556e7e4bad23b018a89c6f240a4f15f3b2540d56
alicanerdogan <aerdogan07@gmail.com>
2015-12-12 09:51:38 +0200

    MultiMediaViewModel becomes a property of VM.

--- Whistle/WhistleGUI/ViewModel/TweetViewModel.cs
+++ Whistle/WhistleGUI/ViewModel/TweetViewModel.cs
@@ -8,17 +8,21 @@ using System.Net;
 using System.Net.Cache;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Documents;
 using System.Windows.Media.Imaging;
 using TwitterLibrary;
+using WhistleGUI.Converter;
 
 namespace WhistleGUI.ViewModel
 {
     public class TweetViewModel : ReactiveObject
     {
         #region Properties
-        public Tweet Tweet { get; private set; }
+        public ITweet Tweet { get; private set; }
         public string Content { get { return Tweet.RawContent; } }
+        public InlineCollection ProcessedContent { get { return MarkupTweetConverter.Convert(Tweet); } }
         public string Username { get { return Tweet.Owner.Name; } }
+        public string ScreenName { get { return Tweet.Owner.ScreenName; } }
         public string TimeTag { get { return Tweet.RelativeTime; } }
         public bool IsLiked { get { return false; } }
         public bool IsRetweeted { get { return false; } }
@@ -32,12 +36,15 @@ namespace WhistleGUI.ViewModel
             get { return _Avatar; }
             set { this.RaiseAndSetIfChanged(ref _Avatar, value); }
         }
+
+        public MultiMediaViewModel MultiMediaViewModel { get; private set; }
         #endregion
 
-        public TweetViewModel(Tweet tweet)
+        public TweetViewModel(ITweet tweet)
         {
             Tweet = tweet;
             LoadAvatar(Tweet.Owner.AvatarURL);
+            MultiMediaViewModel = new MultiMediaViewModel(Tweet.Media);
         }
 
         private async void LoadAvatar(string url)
