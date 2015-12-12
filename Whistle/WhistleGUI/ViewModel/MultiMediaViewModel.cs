0608e51b8cf976fcd28250b5be04c5f122729582
alicanerdogan <aerdogan07@gmail.com>
2015-12-12 09:43:47 +0200

    Initial commit.

---
+++ Whistle/WhistleGUI/ViewModel/MultiMediaViewModel.cs
@@ -0,0 +1,29 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using TwitterLibrary;
+using TwitterLibrary.Entity;
+
+namespace WhistleGUI.ViewModel
+{
+    public class MultiMediaViewModel : ReactiveObject
+    {
+        public ReactiveList<BitmapImage> Images { get; private set; }
+
+        public MultiMediaViewModel(IEnumerable<Media> media)
+        {
+            Images = new ReactiveList<BitmapImage>();
+            media.ToList().ForEach(m => LoadImageAsync(m));
+        }
+
+        private async void LoadImageAsync(Media media)
+        {
+            var bitmap = await Helper.BitmapDownloader.DownloadAsync(media.Destination.ToString());
+            Images.Add(bitmap);
+        }
+    }
+}
