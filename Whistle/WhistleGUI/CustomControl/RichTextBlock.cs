08bba9ec88d177fe4e4dc5dc5d1a5083e02e5072
alicanerdogan <aerdogan07@gmail.com>
2015-12-12 09:48:18 +0200

    RichTextBlock and MultiMediaView has been added to project.

---
+++ Whistle/WhistleGUI/CustomControl/RichTextBlock.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace WhistleGUI.CustomControl
+{
+    public class RichTextBlock : TextBlock
+    {
+
+
+        public InlineCollection RichText
+        {
+            get { return (InlineCollection)GetValue(RichTextProperty); }
+            set { SetValue(RichTextProperty, value); }
+        }
+
+        public static readonly DependencyProperty RichTextProperty =
+            DependencyProperty.Register("RichText", typeof(InlineCollection), typeof(RichTextBlock),
+                new FrameworkPropertyMetadata(null, (dpObj, args) =>
+                {
+                    if (args.NewValue != args.OldValue)
+                    {
+                        var textBlock = dpObj as RichTextBlock;
+                        var inlines = args.NewValue as InlineCollection;
+                        Inline[] array = inlines.ToArray();
+
+                        textBlock.Inlines.Clear();
+
+                        foreach (var inline in array)
+                        {
+                            textBlock.Inlines.Add(inline);
+                        }
+                    }
+                })
+            );
+
+
+    }
+}
---
+++ Whistle/WhistleGUI/View/MultiMediaView.xaml.cs
@@ -0,0 +1,45 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WhistleGUI.ViewModel;
+
+namespace WhistleGUI.View
+{
+    /// <summary>
+    /// Interaction logic for MultiMediaView.xaml
+    /// </summary>
+    public partial class MultiMediaView : UserControl, IViewFor<MultiMediaViewModel>
+    {
+        public MultiMediaView()
+        {
+            InitializeComponent();
+
+            this.OneWayBind(ViewModel, vm => vm.Images, v => v.Media.ItemsSource);
+        }
+
+        #region IViewFor Extension
+        public MultiMediaViewModel ViewModel
+        {
+            get { return (MultiMediaViewModel)GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
+        }
+
+        public static readonly DependencyProperty ViewModelProperty =
+            DependencyProperty.Register("ViewModel", typeof(MultiMediaViewModel), typeof(MultiMediaView), new PropertyMetadata(null));
+
+        object IViewFor.ViewModel { get { return ViewModel; } set { ViewModel = value as MultiMediaViewModel; } }
+        #endregion
+    }
+}
