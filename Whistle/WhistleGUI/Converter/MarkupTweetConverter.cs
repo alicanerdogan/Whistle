de1a65e008573689c4c9477310da45d11208fef0
alicanerdogan <aerdogan07@gmail.com>
2015-12-29 20:51:47 +0200

    MarkupTweetConverter has been refactored.

--- Whistle/WhistleGUI/Converter/MarkupTweetConverter.cs
+++ Whistle/WhistleGUI/Converter/MarkupTweetConverter.cs
@@ -15,17 +15,64 @@ namespace WhistleGUI.Converter
     {
         public static InlineCollection Convert(ITweet tweet)
         {
-            string tweetContent = tweet.Content;
+            string tweetContent = RemoveMediaLinksFromContent(tweet.Content, tweet.Media);
+            var orderedURLs = GetSortedURLsByAppereanceOrder(tweetContent, tweet.HashTags, tweet.Mentions, tweet.URLs);
 
             var paragraph = new Paragraph();
+            int pointer = 0;
 
+            foreach (var url in orderedURLs)
+            {
+                int linkIndex = tweetContent.IndexOf(url.LinkText, StringComparison.InvariantCultureIgnoreCase);
+                var preText = tweetContent.Substring(pointer, linkIndex - pointer);
+                paragraph.Inlines.Add(new Run(preText));
+                Hyperlink hyperlink = CreateHyperlink(url);
+                paragraph.Inlines.Add(hyperlink);
+                pointer = linkIndex + url.LinkText.Length;
+            }
+
+            if (paragraph.Inlines.Count <= 0)
+            {
+                paragraph.Inlines.Add(new Run(tweetContent));
+            }
+
+            return paragraph.Inlines;
+        }
+
+        private static Hyperlink CreateHyperlink(URL url)
+        {
+            var hyperlink = new Hyperlink();
+            if (url is HashTag || url is Mention)
+            {
+                hyperlink.Inlines.Add(url.LinkText);
+            }
+            else
+            {
+                hyperlink.Inlines.Add(url.Destination.Host + url.Destination.PathAndQuery);
+            }
+            hyperlink.NavigateUri = url.Destination;
+            hyperlink.RequestNavigate += (sender, e) => Process.Start(e.Uri.ToString());
+            hyperlink.TextDecorations = null;
+            return hyperlink;
+        }
+
+        private static string RemoveMediaLinksFromContent(string tweetContent, IEnumerable<Media> mediaLinks)
+        {
+            foreach (var media in mediaLinks)
+            {
+                tweetContent = tweetContent.Replace(media.LinkText, String.Empty);
+            }
+            return tweetContent;
+        }
+
+        private static IEnumerable<URL> GetSortedURLsByAppereanceOrder(string tweetContent, IEnumerable<HashTag> hashtags, IEnumerable<Mention> mentions, IEnumerable<URL> urls)
+        {
             var orderedURLs = new List<URL>();
 
             var allURLs = new List<URL>();
-            allURLs.AddRange(tweet.Media);
-            allURLs.AddRange(tweet.HashTags);
-            allURLs.AddRange(tweet.URLs);
-            allURLs.AddRange(tweet.Mentions);
+            allURLs.AddRange(hashtags);
+            allURLs.AddRange(urls);
+            allURLs.AddRange(mentions);
 
             if (allURLs.Count > 0)
             {
@@ -33,28 +80,9 @@ namespace WhistleGUI.Converter
                 allURLs.ForEach(url => dict.Add(new KeyValuePair<URL, int>(url, tweetContent.IndexOf(url.LinkText, StringComparison.InvariantCultureIgnoreCase))));
                 dict.Sort((pair1, pair2) => pair1.Value.CompareTo(pair2.Value));
                 dict.ForEach(pair => orderedURLs.Add(pair.Key));
-
-                int pointer = 0;
-                foreach (var url in orderedURLs)
-                {
-                    int linkIndex = tweetContent.IndexOf(url.LinkText, StringComparison.InvariantCultureIgnoreCase);
-                    var preText = tweetContent.Substring(pointer, linkIndex - pointer);
-                    paragraph.Inlines.Add(new Run(preText));
-                    var hyperlink = new Hyperlink();
-                    hyperlink.Inlines.Add(url.LinkText);
-                    hyperlink.NavigateUri = url.Destination;
-                    hyperlink.RequestNavigate += (sender, e) => Process.Start(e.Uri.ToString());
-                    hyperlink.TextDecorations = null;
-                    paragraph.Inlines.Add(hyperlink);
-                    pointer = linkIndex + url.LinkText.Length;
-                }
-            }
-            else
-            {
-                paragraph.Inlines.Add(new Run(tweetContent));
             }
 
-            return paragraph.Inlines;
+            return orderedURLs;
         }
     }
 }
